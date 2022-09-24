c09feea3db5f7992116e458f304d67743ede22a7
santhoshravinr <113429421+santhoshravinr@users.noreply.github.com>
2022-09-24 23:54:44 +0530

    Delete python program.py

--- python program.py
+++
@@ -1,8 +0,0 @@
-import random
-Temperature = random.randint(250,1500)  
-if(Temperature > 500 ):
-    print("ALARM DETECTED")
-    print(Temperature)
-else:
-    print("EVERYTHING LOOKS GOOD")
-    print(Temperature)
